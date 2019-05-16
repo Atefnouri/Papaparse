b771f3cf8ff37b8075053e0aa06a52e588a2148e
Atef Nouri <nouriatef.isa@gmail.com>
2019-05-16 22:58:28 +0100

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,44 @@ export class AppComponent {
 
     const csvData = '"Hello","World!"';
         
-    this.papa.parse(csvData,{
+   /* this.papa.parse(csvData,{
         complete: (result) => {
             console.log('Parsed: ', result);
         }
-    });
+    });*/
 
   }
+
+  //Call the Convert Function When The File was selected
+ convertFile(input) {
+
+  this.papa.parse(input.files[0], {
+    complete: function(results) {
+      console.log(results);
+      var data = this.papa.parse(results);
+      console.log(data);
+    }
+  });
+  /*const reader = new FileReader();
+  reader.readAsText(input.files[0]);
+  reader.onload = () => {
+    let text = reader.result;
+    console.log( text );
+    
+    let csvData = text;
+    let options = {
+        complete: (results, file) => {
+            console.log('Parsed: ', results, file);
+        }
+        // Add your options here
+    };
+
+    this.papa.parse(csvData,options);
+}
+
+
+  };*/
+}
+ 
 
 }
